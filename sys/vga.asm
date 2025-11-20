2c430b15316ec56519e418c812937461a80a334b
David Roman <officialdavidromanyt@gmail.com>
2025-11-20 19:17:18 -0400

    vga cookup

--- sys/vga.asm
+++ sys/vga.asm
@@ -8,6 +8,10 @@
 
 section .text 
 
+%define max_cols 45
+%define max_rows 80
+
+
 vga_addr dd 0xB8000
 
 vga_index_reg  equ 0x3D4
@@ -16,37 +20,112 @@ vga_off_low    equ 0x0F
 vga_off_high   equ 0x0E
 
 
-max_rows db 45
-max_cols db 80 
 color db 0x0f
 
 control_row db 0
 
 
 control_line:
+    ;1 arg, row
+
     mov control_row, edi
 
 set_cursor:
-    div edi, 2
+    ;1 arg offset
 
-    mov edi,vga_index_reg
-    mov esi, vga_off_high
+
+    ;div offset 2
+    shr edi, 1
+    mov ebx, edi
+
+
+    mov edi, vga_index_reg    
+    mov al, vga_off_high        
     call outb
 
+
+    mov edi, vga_data_reg       
+    mov al, bh ;high byte off
+    call outb
+
+
+    mov edi, vga_index_reg
+    mov al, vga_off_low         
+    call outb
+
+  
+    mov edi, vga_data_reg
+    mov al, bl ;low byte off
+    call outb
+
+    ret
 
 
 
 get_cursor: 
+    ;ret offset eax
+    
+    xor esi, esi
+
+    mov edi, vga_index_reg    
+    mov al, vga_off_high        
+    call outb
+
+
+    push dword vga_data_reg
+    call inb
+    pop edx  
+    shl eax, 8 ;high byte
+    mov ebx, eax
+
+
+    mov edi, vga_index_reg
+    mov al, vga_off_low
+    call outb
+
+
+    push dword vga_data_reg
+    call inb
+    pop edx  
+    add ebx, eax 
+
+    shl ebx, 1 ;*2
+    mov eax, ebx   
+
+
 
 get_offset:
+    ;2 args col, row
+
+    mov eax, [esp+8] ;row
+    imul eax, max_cols
+    add eax, [esp+4] ;+= col
+    shl eax, 1
+    ret
+
+
 
 set_char:
+    ;2 args
+    ;store char in al
+    ;store offset in edi
 
-
-print:
+    mov ebx, vga_addr
+    add ebx, edi ;move to offset 
+    mov [ebx], al ;char
+    mov byte [ebx+1], 0x07; attr
 
 
 scroll_screen:
+    ;1 arg mem_offset
+
+
+
+print:
+    ;1 arg str
+
+    
+
 
 
 clear_screen:
